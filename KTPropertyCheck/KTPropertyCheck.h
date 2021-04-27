d31822e48a9aaff975ed8d1040e7a1458f4ef03c
Kam <Kamtoto.au@gmail.com>
2021-04-27 20:32:16 +0800

    Initial Commit

---
+++ KTPropertyCheck/KTPropertyCheck.h
@@ -0,0 +1,12 @@
+//
+//  KTPropertyCheck.h
+//  KTPropertyCheck
+//
+//  Created by Kam on 2021/4/27.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KTPropertyCheck : NSObject
+
+@end
---
+++ KTPropertyCheck/KTPropertyCheck.m
@@ -0,0 +1,12 @@
+//
+//  KTPropertyCheck.m
+//  KTPropertyCheck
+//
+//  Created by Kam on 2021/4/27.
+//
+
+#import "KTPropertyCheck.h"
+
+@implementation KTPropertyCheck
+
+@end
